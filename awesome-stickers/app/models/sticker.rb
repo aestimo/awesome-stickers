affb81be90ca18529b685b629ef4ad07a02c477c
aestimo <lee.njuguna@gmail.com>
2014-05-27 11:22:30 +0300

    uploads now working...

--- awesome-stickers/app/models/sticker.rb
+++ awesome-stickers/app/models/sticker.rb
@@ -1,4 +1,8 @@
 class Sticker < ActiveRecord::Base
+  require 'carrierwave/orm/activerecord'
+
+  # before_save PhotoUploader::extension_white_list
+
   # use carrierwave to upload photos of the sticker
   mount_uploader :photo, PhotoUploader
 end
