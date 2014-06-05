679f420e384810c9a8acfe4ba8c2882301aac863
aestimo <lee.njuguna@gmail.com>
2014-06-05 14:16:29 +0300

    working on the integrated pages controller...

--- awesome-stickers/config/routes.rb
+++ awesome-stickers/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+
   resources :stickers
 
   root 'home#index'
 
   resources :products
+
+  # these are the other routes
+  PagesController.action_methods.each do |action|
+    get "/#{action}", to: "pages##{action}", as: "#{action}_page"
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
